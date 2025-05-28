ed4b0852f38104beb6f325ee892b79fc790b3021
shatoreuwu <shatore@proton.me>
2025-05-28 17:24:15 +0200

    Koniec chyba

--- part 3/phonebookBackend/index.js
+++ part 3/phonebookBackend/index.js
@@ -1,12 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
-const cors = require('cors')
 
 app.use(express.json())
 app.use(express.static('dist'))
 app.use(morgan('tiny'))
-app.use(cors())
 
 
 let persons = [
