cefdda778f1ae0f1ecb568456510c6d1d5b9043e
shatoreuwu <shatore@proton.me>
2025-06-12 22:46:07 +0200

    postep

--- part 3/phonebookBackend/index.js
+++ part 3/phonebookBackend/index.js
@@ -1,11 +1,34 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+// const mongoose = require('mongoose')
+const Person = require('./models/person')
 
 app.use(express.json())
 app.use(express.static('dist'))
 app.use(morgan('tiny'))
 
+// const password = process.argv[2]
+// const url = `mongodb+srv://fullstack:${password}@cluster0.p0cyfof.mongodb.net/personApp?retryWrites=true&w=majority&appName=Cluster0`
+
+// mongoose.set('strictQuery',false)
+// mongoose.connect(url)
+
+// const personSchema = new mongoose.Schema({
+//     name: String,
+//     number: String,
+//   })
+
+// personSchema.set('toJSON', {
+//   transform: (document, returnedObject) => {
+//     returnedObject.id = returnedObject._id.toString()
+//     delete returnedObject._id
+//     delete returnedObject.__v
+//   }
+// })
+
+// const Person = mongoose.model('Person', personSchema)
 
 let persons = [
     { 
@@ -30,20 +53,18 @@ let persons = [
     }
 ]
 
+
 app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
     response.json(persons)
   })
+})
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const person = persons.find(person => person.id === id)
-    
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
+  Person.findById(request.params.id).then(person => {
+    response.json(person)
   })
+})
 
 app.delete('/api/persons/:id', (request, response) => {
     const id = request.params.id
@@ -78,10 +99,38 @@ const generateId = () => {
   return String(Math.round(randomId))
 }
 
-app.post('/api/persons', (request, response) => {
+// app.post('/api/persons', (request, response) => {
+//   const body = request.body
+
+//   if (!body.name || !body.number) {
+//     return response.status(400).json({ 
+//       error: 'Name and number cannot be empty' 
+//     })
+//   }
+
+
+//   const nameExists = persons.some(person => person.name === body.name);
+//   if (nameExists) {
+//     return response.status(400).json({ 
+//       error: 'Name must be unique' 
+//     });
+//   }
+
+//   const person = {
+//     name: body.name,
+//     number: body.number,
+//     id: generateId(),
+//   }
+
+//   persons = persons.concat(person)
+
+//   response.json(person)
+// })
+
+app.post('/api/person', (request, response) => {
   const body = request.body
 
-  if (!body.name || !body.number) {
+ if (!body.name || !body.number) {
     return response.status(400).json({ 
       error: 'Name and number cannot be empty' 
     })
@@ -103,10 +152,13 @@ app.post('/api/persons', (request, response) => {
 
   persons = persons.concat(person)
 
-  response.json(person)
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
 
-const PORT = process.env.PORT || 3001
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
